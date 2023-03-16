386735b87db7167d6a14c82b922270266bfa732f
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2023-03-16 12:10:34 +0100

    fix: correction du client.controller.js recuperation de req, res

--- clients/routes/client.route.js
+++ clients/routes/client.route.js
@@ -2,9 +2,13 @@ const express = require("express");
 const routes = express.Router();
 const clientController = require("../controllers/client.controller");
 
-routes.get("", clientController.getClients(req, res));
+routes.get("", function (req, res) {
+  clientController.getClients(req, res);
+});
 
-routes.get("/chambres", clientController.getChambres(req, res));
+routes.get("/chambres", function (req, res) {
+  clientController.getChambres(req, res);
+});
 
 module.exports = {
   routes,
